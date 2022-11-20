532d0c5b739b8e537c3d2a1d5ad83416ea8f9191
AIWalaBro <aiwalabro@ail.com>
2022-11-20 20:42:23 +0530

    comment added

--- main.py
+++ main.py
@@ -3,4 +3,4 @@ from sensor.configuration.mongo_db_connection import MongoDBClient
 if __name__ == '__main__':
     mongodb_client = MongoDBClient()
     print("collection_name :", mongodb_client.database.list_collection_names())
-  
+  # comment added 
